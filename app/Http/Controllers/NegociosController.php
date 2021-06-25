1d4e4eb19a061ad678b055b234839256bd6d2f3d
lucasgio <mikaelosoltes@gmail.com>
2021-06-25 00:06:08 -0400

    Fix import

--- app/Http/Controllers/NegociosController.php
+++ app/Http/Controllers/NegociosController.php
@@ -7,6 +7,7 @@ use App\Models\Negocios;
 use App\Models\Categoria;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
 
 class NegociosController extends Controller
 {
